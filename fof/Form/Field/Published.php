d805b525966356deb496d0f42a43290032c790b9
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-03-02 19:45:05 +0200

    Refactoring the Form package
    Merging Joomla and FOF field classes

--- fof/Form/Field/Published.php
+++ fof/Form/Field/Published.php
@@ -20,75 +20,8 @@ defined('_JEXEC') or die;
  * Form Field class for FOF
  * Supports a generic list of options.
  */
-class Published extends \JFormFieldList implements FieldInterface
+class Published extends BaseList implements FieldInterface
 {
-	/**
-	 * @var  string  Static field output
-	 */
-	protected $static;
-
-	/**
-	 * @var  string  Repeatable field output
-	 */
-	protected $repeatable;
-
-	/**
-	 * The Form object of the form attached to the form field.
-	 *
-	 * @var    Form
-	 */
-	protected $form;
-
-	/**
-	 * A monotonically increasing number, denoting the row number in a repeatable view
-	 *
-	 * @var  int
-	 */
-	public $rowid;
-
-	/**
-	 * The item being rendered in a repeatable form field
-	 *
-	 * @var  DataModel
-	 */
-	public $item;
-
-	/**
-	 * Method to get certain otherwise inaccessible properties from the form field object.
-	 *
-	 * @param   string  $name  The property name for which to the the value.
-	 *
-	 * @return  mixed  The property value or null.
-	 *
-	 * @since   2.0
-	 */
-	public function __get($name)
-	{
-		switch ($name)
-		{
-			case 'static':
-				if (empty($this->static))
-				{
-					$this->static = $this->getStatic();
-				}
-
-				return $this->static;
-				break;
-
-			case 'repeatable':
-				if (empty($this->repeatable))
-				{
-					$this->repeatable = $this->getRepeatable();
-				}
-
-				return $this->repeatable;
-				break;
-
-			default:
-				return parent::__get($name);
-		}
-	}
-
 	/**
 	 * Method to get the field options.
 	 *
@@ -175,23 +108,6 @@ class Published extends \JFormFieldList implements FieldInterface
 		}
 
 		return $stack;
-	}
-
-	/**
-	 * Get the rendering of this field type for static display, e.g. in a single
-	 * item view (typically a "read" task).
-	 *
-	 * @since 2.0
-	 *
-	 * @return  string  The field HTML
-	 */
-	public function getStatic()
-	{
-		$class = $this->class ? ' class="' . $this->class . '"' : '';
-
-		return '<span id="' . $this->id . '" ' . $class . '>' .
-			htmlspecialchars(GenericList::getOptionName($this->getOptions(), $this->value), ENT_COMPAT, 'UTF-8') .
-			'</span>';
 	}
 
 	/**
